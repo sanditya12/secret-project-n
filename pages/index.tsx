349be748e6287d99b5e69d589d20dffc3eb60c05
sanditya12 <sanditya2002@gmail.com>
2022-11-29 14:59:03 +0100

    fix favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
     <div className=" bg-gradient-to-b from-transparent to-dark ">
       <Head>
         <title>Shalamanya</title>
-        <link rel="icon" href="/logoshalamanya_close" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="min-h-[95vh]">
